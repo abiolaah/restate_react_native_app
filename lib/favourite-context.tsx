43d00c3af0a3ad0b975e1d6d46b19fd3cb25f5e0
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-04-07 16:07:46 -0600

    Refactor to trigger toast and notification

--- lib/favourite-context.tsx
+++ lib/favourite-context.tsx
@@ -1,6 +1,8 @@
 // lib/favourites-context.tsx
 import React, {createContext, useState, useContext, useEffect} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNotifications} from "@/lib/notification-context";
+import Toast from "react-native-toast-message";
 
 type FavouriteContextType = {
     favourites: FavouritesProps[];
@@ -20,6 +22,7 @@ const FavouriteContext = createContext<FavouriteContextType>({
 
 export const FavouritesProvider = ({ children }: { children: React.ReactNode }) => {
     const [favourites, setFavourites] = useState<FavouritesProps[]>([]);
+    const {addNotification} = useNotifications();
 
     // Load notifications from storage on mount
     useEffect(() => {
@@ -53,6 +56,17 @@ export const FavouritesProvider = ({ children }: { children: React.ReactNode }) 
             // Check if property already exists in wishlist
             const exists = prev.some(item => item.property.$id === property.$id);
             if (!exists) {
+                Toast.show({
+                    type: 'success',
+                    text1: 'Success',
+                    text2: 'Added to Favourites successfully',
+                })
+                addNotification({
+                    title: 'Added to Favourites',
+                    message: `${property.name} has been added to Favourites`,
+                    type: 'system',
+                    relatedId: property.$id
+                })
                 return [
                     ...prev,
                     {
@@ -67,7 +81,23 @@ export const FavouritesProvider = ({ children }: { children: React.ReactNode }) 
     };
 
    const removeFromFavourites =  (propertyId: string) => {
-       setFavourites(prev => prev.filter(item => item.property.$id !== propertyId));
+       setFavourites(prev => {
+           const removedProperty = prev.find(item => item.property.$id === propertyId);
+           if (removedProperty) {
+               Toast.show({
+                   type: 'success',
+                   text1: 'Success',
+                   text2: 'Removed from Favourites successfully',
+               })
+               addNotification({
+                   title: 'Removed from Favorites',
+                   message: `${removedProperty.property.name} has been removed from your favorites`,
+                   type: 'system',
+                   relatedId: propertyId
+               });
+           }
+           return prev.filter(item => item.property.$id !== propertyId);
+       });
    }
 
    const isInFavourites = (propertyId: string) => {
